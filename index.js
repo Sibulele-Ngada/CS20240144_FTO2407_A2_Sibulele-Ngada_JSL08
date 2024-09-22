439355fc7b03afa300704f530846580c1bd31a50
Sibulele Ngada <sibulelengada@gmail.com>
2024-09-22 03:39:46 +0200

    Update constructor to ensure only 1 instance is returned

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-const bankBranchInstance = new BankBranch();
+let bankBranchInstance;
 // 2. Define a class called `BankBranch` for managing branch information.
 class BankBranch {
     // 3. In the `BankBranch` class:
-    //    - Create a constructor that takes `branchInfo` as a parameter.
-    //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
-    //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
-    //    - Return the `bankBranchInstance` whether it's newly created or existing.
+    //    - Create a constructor that takes `branchInfo` as a parameter.  
     constructor(branchInfo){
         this.branchInfo = branchInfo;
-        if (bankBranchInstance.instance === null){
-            bankBranchInstance.instance = this;
+        //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
+        if (!bankBranchInstance){
+            //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
+            bankBranchInstance = this;
         }
-        return bankBranchInstance.instance;
+        Object.freeze(bankBranchInstance);
+        //    - Return the `bankBranchInstance` whether it's newly created or existing.
+        return bankBranchInstance;
     }
-    
+
     // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
     getBranchInfo(){
-        return branchInfo;
+        return this.branchInfo;
     }
 }
 
-// 5. In the usage section:
-//    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
+
+
 //    - Use the `getBranchInfo` method to retrieve branch information from the instances.
 //    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
 
